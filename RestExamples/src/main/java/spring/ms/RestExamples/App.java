16d065903d5e046581e3a753dadb04ee8c2bfd4c
Omar Villalpando <g_omar_v@live.com.mx>
2018-09-08 00:22:25 -0500

    Hello world Spring boot
    Proyecto eclipse con ejmplo Hello World en Spring Boot

---
+++ RestExamples/src/main/java/spring/ms/RestExamples/App.java
@@ -0,0 +1,13 @@
+package spring.ms.RestExamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
